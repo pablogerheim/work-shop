78526507244031e4897541d93c95c4c1783c6ef9
Pablo <pablogerheim@gmail.com>
2022-08-17 21:49:35 -0300

    Docker

--- front/src/pages/adm/admHome.jsx
+++ front/src/pages/adm/admHome.jsx
@@ -14,7 +14,7 @@ function AdmHome() {
     return (
         <div className='screen home' >
             <div className='flex justify-evenly z-10'>
-                <h2 className='pt-2 pl-2 z-10'>ADM home </h2>
+                <h2 className='pt-2 pl-2 z-10'>ADM home  </h2>
                 <button onClick={() => handleLogout()} className='border rounded bg-slate-100 mt-2 px-2'> Deslogar !</button>
             </div>
             <div className='flex justify-center'>
